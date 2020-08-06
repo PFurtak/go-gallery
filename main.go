e03a0ac9814045be2730dd217d4b23cc21fd5d78
Patrick Furtak <patrick@patrickfurtak.com>
2020-08-05 21:04:18 -0400

    Added 404 page and wrote 404 status code response

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ func handlerFunc(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
 		fmt.Fprint(rw, "<h1>Welcome to Go-Gallery!</h1>")
-	} else {
+	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(rw, "To get in touch, please email: <a href=\"mailto:support@gogallery.com\">support@gogallery.com</a>")
+	} else {
+		rw.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(rw, "<h1>404 Page not found ;(</h1>")
 	}
 }
 
