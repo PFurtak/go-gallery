b52f9fab452602029fc80faf952d64e1709dfd58
Patrick Furtak <patrick@patrickfurtak.com>
2020-08-07 23:26:29 -0400

    Created custom 404 page with Gorilla serve mux

--- main.go
+++ main.go
@@ -22,8 +22,15 @@ func faqHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(rw, "<h1>FAQ</h1><br><ol><li>Who is this site for? <b>Photographers!</b></li><li>Can I upload my own photos? <b>Yes!</b></li><li>What language is this application written in? <b>Golang</b></li></ol>")
 }
 
+func notFound(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-type", "text/html")
+	rw.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(rw, "<h1>404</h1><br><h3>Page not found :[</h3>")
+}
+
 func main() {
 	router := mux.NewRouter()
+	router.NotFoundHandler = http.HandlerFunc(notFound)
 	router.HandleFunc("/", homeHandler)
 	router.HandleFunc("/contact", contactHandler)
 	router.HandleFunc("/faq", faqHandler)
