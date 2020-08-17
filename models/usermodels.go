0916778f01caec577eb930de91fe563f09054391
Patrick Furtak <patrick@patrickfurtak.com>
2020-08-17 15:43:07 -0400

    Brought in bcrypt package, wrote logic to has user passwords

--- models/usermodels.go
+++ models/usermodels.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -33,8 +35,10 @@ type UserService struct {
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
+	Name         string
+	Email        string `gorm:"not null;unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
 
 // ByID is used to look up user by provided ID
@@ -68,6 +72,12 @@ func first(db *gorm.DB, dst interface{}) error {
 
 // Create will create the provided user
 func (us *UserService) Create(user *User) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = string(hashedBytes)
+	user.Password = ""
 	return us.db.Create(user).Error
 }
 
